2d2190ff446327a98c6adb42dff58539cba8dee6
Marek Wieiwórka <marek.wiewiorka@gmail.com>
2025-08-17 09:49:51 +0200

    polars-utils fix

--- crates/polars-utils/src/select.rs
+++ crates/polars-utils/src/select.rs
@@ -1,6 +1,10 @@
 #[cfg(feature = "nightly")]
 pub fn select_unpredictable<T>(cond: bool, true_val: T, false_val: T) -> T {
-    cond.select_unpredictable(true_val, false_val)
+    if cond {
+        true_val
+    } else {
+        false_val
+    }
 }
 
 #[cfg(not(feature = "nightly"))]
